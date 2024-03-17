8c683cf804b67d84b27d1ab6b8e3ef82a573968b
Egomez-2021340 <egomez-2021340@kinal.edu.gt>
2024-03-17 08:28:41 -0600

    Se termina estructura html y css

--- assets/logicaLogin.js
+++ assets/logicaLogin.js
@@ -1,4 +1,12 @@
-const username = document.getElementById('username')
-const password = document.getElementById('password') 
-const button = document.getElementById('button')
+const button = document.getElementById('button');
 
+button.addEventListener('click', () => {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username.trim() !== '' && password.trim() !== '') {
+        window.location.href = "../paginaPrincipal.html"; 
+    } else {
+        alert("Por favor ingresa usuario y contraseña");
+    }
+});
---
+++ assets/logicaPrincipal.js
@@ -0,0 +1,20 @@
+const nombre = document.querySelector('.nombre')
+const telefono = document.querySelector('.telefono')
+const direccion = document.querySelector('.direccion')
+const btnAgregarTarea = document.querySelector('.btn-agregar-tarea')
+
+const listadoTareas = document.querySelector('.listado-tareas')
+
+const db = window.localStorage
+
+btnAgregarTarea.onclick = () => {
+    let contacto = {
+        id: Math.random(1, 100),
+        nombre: nombre.value,
+        telefono: telefono.value,
+        direccion: direccion.value,
+    }
+
+    console.log(contacto)
+}
+
